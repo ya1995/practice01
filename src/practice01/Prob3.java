c865cfa427bd954e03a76b79cd9286e888f8f694
bit-user <bit-user@bit>
2017-04-14 16:29:28 +0900

    solved

--- src/practice01/Prob3.java
+++ src/practice01/Prob3.java
@@ -1,7 +1,26 @@
 package practice01;
 
+import java.util.Scanner;
+
 public class Prob3 {
 
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner( System.in  );
+
+		while( true ) {
+			System.out.print( "수를 입력하시오 : " );
+			int number = scanner.nextInt();
+
+			int sum = 0;
+			for( int i = 1; i <= number; i++ ) {
+				if( ( number % 2 == 0  && i % 2 == 0)  ||
+					( number % 2 != 0   && i % 2 != 0 ) ) {
+					sum += i;
+				}
+			}
+			
+			System.out.println( "결과값 : " + sum );
+		}
 	}
 }
--- src/practice01/Prob5.java
+++ src/practice01/Prob5.java
@@ -1,8 +1,30 @@
 package practice01;
 
 public class Prob5 {
-
 	public static void main(String[] args) {
-
+		for( int i = 1; i <= 999; i++ ) {
+			
+			String s = String.valueOf( i );
+			int length = s.length();
+			int clap = 0;
+			
+			for( int j = 0; j < length; j++ ) {
+				char c = s.charAt( j );
+				if( c == '3' || c == '6' || c == '9' ) {
+					clap++;
+				}
+			}
+			
+			if( clap == 0 ) {
+				continue;
+			}
+			
+			System.out.print( s + " " );
+			for( int j = 0; j < clap; j++ ) {
+				System.out.print( "짝" );
+			}
+			
+			System.out.print( "\n" );
+		}
 	}
 }
